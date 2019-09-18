9101f58de62df8fdfcfb970a93349c5afd1cf35f
benlowry <benlowry@outlook.com>
2019-09-18 10:27:55 -0600

    Removed dev dependencies

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
+if [ ! -d node_modules/puppeteer ]; then
+  npm install puppeteer --no-save
+fi
+
+if [ ! -d node_modules/@userdashboard/dashboard ]; then
+  npm install @userdashboard/dashboard --no-save
+fi
+
 NODE_ENV=testing \
 FAST_START=true \
 PORT=8003 \
--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,5 +1,3 @@
-npm update @userdashboard/dashboard || exit 1
-npm update puppeteer || exit 1
 npm shrinkwrap --dev || exit 1
 git add npm-shrinkwrap.json || exit 1
 git add package.json || exit 1
