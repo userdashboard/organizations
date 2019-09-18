92bb39498785de4b53896ceed9f47f6146a3048a
benlowry <benlowry@outlook.com>
2019-09-18 11:10:02 -0600

    Optionally inherit pupeteer reference

--- test-helper.js
+++ test-helper.js
@@ -1,5 +1,7 @@
 /* eslint-env mocha */
 global.applicationPath = global.applicationPath || __dirname
+global.puppeteer = global.puppeteer || require('puppeteer')
+
 const TestHelper = require('@userdashboard/dashboard/test-helper.js')
 
 module.exports = {
