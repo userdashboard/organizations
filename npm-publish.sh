284ba0f7602c950ff38065cf1a8e0c8f7b79ed4d
benlowry <benlowry@outlook.com>
2019-10-08 16:38:40 -0600

    Correctly bump NPM version

--- npm-publish.sh
+++ npm-publish.sh
@@ -5,7 +5,9 @@ PACKAGE=`cat package.json | grep version`
 PACKAGE="${PACKAGE/\"version\": \"/}"
 PACKAGE="${PACKAGE/\",/}"
 PACKAGE="${PACKAGE/  /}"
-if [ "$LATEST" = "$PACKAGE" ]; then
+echo "PACKAGE $PACKAGE LATEST $LATEST |"
+if [ "$LATEST" == "$PACKAGE" ]; then
+  echo "EQUAL, patching"
   VERSION=`npm version patch` || exit 1
 else
   VERSION=$PACKAGE
@@ -13,6 +15,6 @@ fi
 bash test.sh > tests.txt || exit 1
 FAST_START=true NODE_ENV=sitemap node main.js || exit 1
 git add . || exit 1
-git commit -m "Version $VERSION " || exit 1
+git commit -m "Version $VERSION" || exit 1
 git push origin master || exit 1
 npm publish || exit 1
