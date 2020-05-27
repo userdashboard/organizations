73cfe318d8e7a657a1d79de13bf48c3e8b650a96
Ben Lowry <benlowry@pop-os.localdomain>
2020-05-27 15:52:16 -0600

    Clean up old files

--- update-dependencies.sh
+++
@@ -1,5 +0,0 @@
-npm shrinkwrap --dev || exit 1
-git add npm-shrinkwrap.json || exit 1
-git add package.json || exit 1
-git commit -m "Updated dependencies" || exit 1
-git push origin master || exit 1
