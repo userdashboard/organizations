d87feaa1f972642d4d19d033794c121612f11381
Ben Lowry <benlowry@pop-os.localdomain>
2020-05-28 09:04:14 -0600

    Don't initialize puppeteer

--- test-helper.js
+++ test-helper.js
@@ -1,6 +1,5 @@
 /* eslint-env mocha */
 global.applicationPath = global.applicationPath || __dirname
-global.puppeteer = global.puppeteer || require('puppeteer')
 
 const TestHelper = require('@userdashboard/dashboard/test-helper.js')
 
